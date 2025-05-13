63cee0e769e3001b4448bd866124bc6bfbfaf3c4
Abhay Dutt <dutt.abhay@gmail.com>
2025-05-13 17:36:00 +0530

    add sidebar ui

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { useGetAuthUserQuery } from "@/state/api/authEndpoints";
+import Sidebar from "@/components/AppSidebar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+
+  return (
+    <SidebarProvider>
+      <div className="min-h-screen w-full bg-primary-100">
+        <Navbar />
+        <div style={{ marginTop: `${NAVBAR_HEIGHT}px` }}>
+          <main className="flex">
+            <Sidebar userType={authUser?.userRole.toLowerCase()} />
+            <div className="flex-grow transition-all duration-300">
+              {children}
+            </div>
+          </main>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+};
+
+export default DashboardLayout;
